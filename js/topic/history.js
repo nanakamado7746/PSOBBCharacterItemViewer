3adad37f94792c765fc1c94aae69a93b1164a549
nanakamado <nanakamado7746@gmail.com>
2022-04-07 18:28:37 +0900

    通知欄を追加。resourcesを整理。

---
+++ js/topic/history.js
@@ -0,0 +1,6 @@
+{
+  "NOTICE", "link:Forum page:https://www.pioneer2.net/community/threads/character-item-viewer-on-the-web.22882/:"
+  "BUG","Automatic display function of search results is being tested. (検索結果の自動表示機能はテスト中です。)"
+        "Please press search button or reload web page if search results are incorrect. (検索結果が変なときは検索ボタンを押すかWEBページを更新してください。)"
+  "UPDATED","",
+}

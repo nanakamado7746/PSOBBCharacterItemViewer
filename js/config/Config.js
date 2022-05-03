a66874258e1f15273faccedd52292aacf36d9b77
nanakamado <nanakamado7746@gmail.com>
2022-05-03 16:46:53 +0900

    ツールのカテゴリ範囲を変更

--- js/config/Config.js
+++ js/config/Config.js
@@ -13,8 +13,8 @@ class Config {
   BarrierRange = [ 0x010200, 0x0102A5 ];
   UnitRange = [ 0x010300, 0x010364 ];
   MagRange = [ 0x020000, 0x025200 ];
-  ToolRange = [ 0x030000, 0x030B06 ];
-  MesetaRange = [ 0x030000, 0x030102 ];
+  ToolRange = [ 0x030000, 0x030102 ];
+  MesetaRange = [ 0x040000, 0x040000 ];
   DiskRange = [ 0x050000, 0x05121D ];
   EphineaRange = [ 0x031005, 0x031810 ];
   SRankWeaponRange = [ 0x0070, 0x0088 ];

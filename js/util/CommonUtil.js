d510a1aa1a227649f752737082bad40f411dd06c
nanakamado <nanakamado7746@gmail.com>
2022-05-26 00:17:26 +0900

    検証用の名前デコーダーを作成

--- js/util/CommonUtil.js
+++ js/util/CommonUtil.js
@@ -1,7 +1,8 @@
 class CommonUtil {
 
   // 配列をint型にする
-  static binaryArrayToInt(arr){
+  static binaryArrayToInt(arr)
+  {
     let int;
     for (const el of arr)
     {
@@ -11,12 +12,27 @@ class CommonUtil {
   }
 
   // 配列を１６進数文字列にする
-  static binaryArrayToHex(arr){
+  static binaryArrayToHex(arr)
+  {
     let str = '';
     for(const el of arr)
     {
       str += el.toString('16').padStart(2, '0')
     }
     return str.toUpperCase();
+  }
+
+  static StringTocharCodes(str)
+  {
+    let charCodes = "";
+    for (let i = 0; i < str.length; i++)
+    {
+      const char = str.charCodeAt(i);
+      const front = (char & 0xFF00) >> 8;
+      const back = char & 0x00FF;
+      charCodes += back.toString(16) + front.toString(16);
+    }
+    console.log(charCodes);
+    return charCodes.toUpperCase();
   }
 }
